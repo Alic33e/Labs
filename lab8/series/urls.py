a4a7efbd9813cd6821c4ab8f7bb469e2ee0798dd
jeanpool <jp@gmail.com>
2023-10-22 01:50:14 -0500

    2.0

--- lab8/series/urls.py
+++ lab8/series/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^series/$', views.serie_list),
-    url(r'^series/(?P<pk>[0-9]+)/$', views.serie_detail),
+    path('series/', views.serie_list),
+    path('series/<int:pk>/', views.serie_detail),
 ]
